94192d72da34b2d6297958d50eaa5cf39538d467
Luks245 <florekl03c@lo5.bielsko.pl>
2023-06-07 20:35:13 +0200

    implemented timeOfDay changing functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
+import gsap from "gsap";
 import * as CANNON from "cannon-es";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
@@ -10,8 +11,41 @@ sand.repeat.set(10, 10);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xc2e5ee);
-// scene.background = new THREE.Color(0x07011f); night
-// scene.background = new THREE.Color(0xf86e39); sunset
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.5);
+hemiLight.position.set(2000, 200, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(0, 10, 4);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+
+const boom = new THREE.Group();
+boom.add(camera);
+camera.position.set(0, 4, 9);
+camera.lookAt(0, 0, 0);
+scene.add(boom);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setPixelRatio(1);
+renderer.setSize(sizes.width, sizes.height);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.render(scene, camera);
 
 const planeGeometry = new THREE.BoxGeometry(10, 0.2, 10);
 const planeMaterial = new THREE.MeshStandardMaterial({
@@ -37,44 +71,24 @@ const getModel = (url, scale, bodyPosition, bodyQuaternion) => {
   });
 };
 
-// let cactus;
-// const loader = new GLTFLoader();
-// loader.load("./assets/cactus.glb", (glb) => {
-//   glb.scene.scale.setScalar(0.17);
-//   glb.scene.traverse(function (node) {
-//     if (node.isMesh) {
-//       node.castShadow = true;
-//     }
-//   });
-//   cactus = glb.scene;
-//   scene.add(glb.scene);
-// });
-
-// let plant;
-// loader.load("./assets/Small Plant.glb", (glb) => {
-//   glb.scene.scale.setScalar(1);
-//   glb.scene.traverse(function (node) {
-//     if (node.isMesh) {
-//       node.castShadow = true;
-//     }
-//   });
-//   plant = glb.scene;
-//   scene.add(glb.scene);
-// });
-
-// let rock;
-// loader.load("./assets/Desert pebble.glb", (glb) => {
-//   glb.scene.scale.setScalar(15);
-//   glb.scene.traverse(function (node) {
-//     if (node.isMesh) {
-//       node.castShadow = true;
-//     }
-//   });
-//   rock = glb.scene;
-//   scene.add(glb.scene);
-// });
 const loader = new GLTFLoader();
-let ball2;
+
+//underside of the island
+loader.load("./assets/underside.glb", (glb) => {
+  glb.scene.scale.setScalar(11);
+  glb.scene.traverse(function (node) {
+    if (node.isMesh) {
+      node.castShadow = true;
+      node.receiveShadow = true;
+    }
+  });
+  glb.scene.rotateZ(Math.PI);
+  glb.scene.position.set(0, -0.2, 0);
+  scene.add(glb.scene);
+});
+
+//player model
+let player;
 loader.load("./assets/Tumbleweed.glb", (glb) => {
   glb.scene.scale.setScalar(0.7);
   glb.scene.traverse(function (node) {
@@ -82,22 +96,24 @@ loader.load("./assets/Tumbleweed.glb", (glb) => {
       node.castShadow = true;
     }
   });
-  ball2 = glb.scene;
+  player = glb.scene;
   scene.add(glb.scene);
 });
 
-let ball;
-loader.load("./assets/Tumbleweed.glb", (glb) => {
-  glb.scene.scale.setScalar(0.7);
+//Object model
+let object;
+loader.load("./assets/Desert pebble.glb", (glb) => {
+  glb.scene.scale.setScalar(7);
   glb.scene.traverse(function (node) {
     if (node.isMesh) {
       node.castShadow = true;
     }
   });
-  ball = glb.scene;
+  object = glb.scene;
   scene.add(glb.scene);
 });
 
+//CANNON
 const world = new CANNON.World({
   gravity: new CANNON.Vec3(0, -9.81, 0),
 });
@@ -110,26 +126,27 @@ const groundBody = new CANNON.Body({
 });
 world.addBody(groundBody);
 
-const spherePhysMat = new CANNON.Material();
-const sphereBody = new CANNON.Body({
+const objectPhysMat = new CANNON.Material();
+const objectBody = new CANNON.Body({
   mass: 0.05,
-  shape: new CANNON.Sphere(0.5),
+  shape: new CANNON.Box(new CANNON.Vec3(0.4, 0.3, 0.4)),
   position: new CANNON.Vec3(0, 10, 0),
-  material: spherePhysMat,
+  material: objectPhysMat,
 });
-const sphereBody2 = new CANNON.Body({
+const spherePhysMat = new CANNON.Material();
+const playerBody = new CANNON.Body({
   mass: 1,
   shape: new CANNON.Sphere(0.5),
   position: new CANNON.Vec3(0, 5, 1.5),
   material: spherePhysMat,
 });
 
-sphereBody.linearDamping = 0.31;
-sphereBody2.linearDamping = 0.31;
-sphereBody.angularDamping = 0.31;
-sphereBody2.angularDamping = 0.31;
-world.addBody(sphereBody);
-world.addBody(sphereBody2);
+objectBody.linearDamping = 0.31;
+playerBody.linearDamping = 0.31;
+objectBody.angularDamping = 0.31;
+playerBody.angularDamping = 0.31;
+world.addBody(objectBody);
+world.addBody(playerBody);
 
 const cactusBody = new CANNON.Body({
   shape: new CANNON.Box(new CANNON.Vec3(0.4, 2.5, 0.4)),
@@ -178,9 +195,16 @@ world.addBody(goldRocksBody);
 const groundSphereContactMat = new CANNON.ContactMaterial(
   groundPhysMat,
   spherePhysMat,
-  { restitution: 0.6, friction: 0.02 }
+  { restitution: 0.5, friction: 0.02 }
 );
 world.addContactMaterial(groundSphereContactMat);
+
+const groundObjectContactMat = new CANNON.ContactMaterial(
+  groundPhysMat,
+  objectPhysMat,
+  { restitution: 0.2, friction: 0.01 }
+);
+world.addContactMaterial(groundObjectContactMat);
 
 getModel("Cactus", 0.17, cactusBody.position, cactusBody.quaternion);
 getModel("Small Plant", 1, plantBody.position, plantBody.quaternion);
@@ -191,71 +215,24 @@ getModel("Cactus", 0.12, cactusBody2.position, cactusBody2.quaternion);
 getModel("Gold Rocks", 7, goldRocksBody.position, goldRocksBody.quaternion);
 const timeStep = 1 / 60;
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.5);
-hemiLight.position.set(2000, 200, 0);
-scene.add(hemiLight);
-
-const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-dirLight.position.set(0, 10, 4);
-dirLight.castShadow = true;
-scene.add(dirLight);
-
-const camera = new THREE.PerspectiveCamera(
-  45,
-  sizes.width / sizes.height,
-  0.1,
-  1000
-);
-
-const boom = new THREE.Group();
-boom.add(camera);
-camera.position.set(0, 4, 10);
-camera.lookAt(0, 0, 0);
-scene.add(boom);
-
-const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setPixelRatio(2);
-renderer.setSize(sizes.width, sizes.height);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.render(scene, camera);
-
 const loop = () => {
   window.requestAnimationFrame(loop);
   world.step(timeStep);
-  //   groundBody.quaternion.set(0.1, 0, 0, 1);
+
   plane.position.copy(groundBody.position);
   plane.quaternion.copy(groundBody.quaternion);
-  if (ball) {
-    ball.position.copy(sphereBody.position);
-    ball.quaternion.copy(sphereBody.quaternion);
+  if (object) {
+    object.position.copy(objectBody.position);
+    object.quaternion.copy(objectBody.quaternion);
+
+    if (object.position.y < -20) scene.remove(object);
   }
-  if (ball2) {
-    ball2.position.copy(sphereBody2.position);
-    ball2.quaternion.copy(sphereBody2.quaternion);
-    camera.lookAt(ball2.position.x, ball2.position.y, ball2.position.z);
-    boom.position.copy(ball2.position);
+  if (player) {
+    player.position.copy(playerBody.position);
+    player.quaternion.copy(playerBody.quaternion);
+    camera.lookAt(player.position.x, player.position.y, player.position.z);
+    boom.position.copy(player.position);
   }
-  //   if (cactus) {
-  //     // cactusBody.quaternion.set(0.1, 0, 0, 1);
-  //     // cactusBody.position.y = -2 * Math.tan((18 * 2 * Math.PI) / 360);
-  //     cactus.position.copy(cactusBody.position);
-  //     cactus.quaternion.copy(cactusBody.quaternion);
-  //   }
-  //   if (plant) {
-  //     plant.position.copy(plantBody.position);
-  //     plant.quaternion.copy(plantBody.quaternion);
-  //   }
-  //   if (rock) {
-  //     rock.position.copy(rockBody.position);
-  //     rock.quaternion.copy(rockBody.quaternion);
-  //   }
 
   renderer.render(scene, camera);
 };
@@ -289,28 +266,83 @@ window.addEventListener("keydown", () => {
   const cameraDirection = camera.getWorldDirection(new THREE.Vector3());
 
   if (keysMap["w"])
-    sphereBody2.applyImpulse(
+    playerBody.applyImpulse(
       new CANNON.Vec3(cameraDirection.x, 0, cameraDirection.z)
     );
   if (keysMap["s"])
-    sphereBody2.applyImpulse(
+    playerBody.applyImpulse(
       new CANNON.Vec3(-cameraDirection.x, 0, -cameraDirection.z)
     );
   if (keysMap["a"])
-    sphereBody2.applyImpulse(
+    playerBody.applyImpulse(
       new CANNON.Vec3(cameraDirection.z, 0, -cameraDirection.x)
     );
   if (keysMap["d"])
-    sphereBody2.applyImpulse(
+    playerBody.applyImpulse(
       new CANNON.Vec3(-cameraDirection.z, 0, cameraDirection.x)
     );
-  if (keysMap[" "]) sphereBody2.applyImpulse(new CANNON.Vec3(0, 6, 0));
+  if (keysMap[" "]) playerBody.applyImpulse(new CANNON.Vec3(0, 6, 0));
 });
+const overlay = document.getElementById("overlay");
+
+const changeTime = (timeOfDay) => {
+  switch (timeOfDay) {
+    case "noon":
+      gsap.to(dirLight.position, {
+        x: 0,
+        y: 10,
+        z: 4,
+        duration: 0.3,
+      });
+      scene.background.lerpColors(
+        scene.background,
+        new THREE.Color(0xc2e5ee),
+        1
+      );
+      overlay.style.backgroundColor = "transparent";
+      break;
+    case "sunset":
+      gsap.to(dirLight.position, {
+        x: 0,
+        y: 3,
+        z: 13,
+        duration: 0.3,
+      });
+      scene.background.lerpColors(
+        scene.background,
+        new THREE.Color(0xf86e39),
+        1
+      );
+      overlay.style.backgroundColor = "rgba(243, 156, 42, 0.2)";
+      break;
+    case "night":
+      gsap.to(dirLight.position, {
+        x: 0,
+        y: 4,
+        z: 7,
+        duration: 0.3,
+      });
+      scene.background.lerpColors(
+        scene.background,
+        new THREE.Color(0x07011f),
+        1
+      );
+      overlay.style.backgroundColor = "rgba(0, 0, 0,0.5)";
+      break;
+    default:
+      return;
+  }
+};
 
 const sunsetButton = document.getElementById("sunset");
 const nightButton = document.getElementById("night");
 const noonButton = document.getElementById("noon");
 
-sunsetButton.addEventListener("click", changeTime);
-nightButton.addEventListener("click", changeTime);
-noonButton.addEventListener("click", changeTime);
+sunsetButton.addEventListener("click", () => changeTime("sunset"));
+nightButton.addEventListener("click", () => changeTime("night"));
+noonButton.addEventListener("click", () => changeTime("noon"));
+const overlayContent = document.getElementById("overlay-content");
+
+window.onload = () => {
+  overlayContent.style.opacity = "0";
+};
